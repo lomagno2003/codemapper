91c8ed90cf74fafa28c9feb3f6a7d63333903ba4
lomagno2003 <lomagno2003@gmail.com>
2014-07-24 23:54:51 -0300

    Mapper factory

--- src/main/java/com/clomagno/codemapper/MapperFactory.java
+++ src/main/java/com/clomagno/codemapper/MapperFactory.java
@@ -8,37 +8,64 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 public class MapperFactory {
+	private Integer findHeaderIndex(HSSFSheet sheet, String header){
+		Integer result = 0;
+		
+		Iterator<Row> rowIterator = sheet.iterator();
+		Row row = rowIterator.next();
+        //For each row, iterate through all the columns
+        Iterator<Cell> cellIterator = row.cellIterator();
+		while (cellIterator.hasNext()) 
+        {
+            Cell cell = cellIterator.next();
+            switch (cell.getCellType()) 
+            {
+                case Cell.CELL_TYPE_NUMERIC:
+                	if(Double.valueOf(cell.getNumericCellValue()).toString().equals(header)){
+                    	return result;
+                    }
+                    break;
+                case Cell.CELL_TYPE_STRING:
+                    if(cell.getStringCellValue().equals(header)){
+                    	return result;
+                    }
+                    break;
+            }
+            
+            result++;
+        }
+		
+		return null;
+	}
+	
 	public Mapper getMapperFromFiles(HSSFWorkbook configuration){
 		Mapper result = null;
 		
 		HSSFSheet generalConfiguration = configuration.getSheet("Configuracion General");
+        
+        Integer distributorsIndex = findHeaderIndex(generalConfiguration,"Distribuidor");
+        Integer suffixIndex = findHeaderIndex(generalConfiguration,"Sufijo");
+        
 		
 		//Iterate through each rows one by one
         Iterator<Row> rowIterator = generalConfiguration.iterator();
-        
-        
+        rowIterator.next();
         
         while (rowIterator.hasNext()) 
         {
             Row row = rowIterator.next();
-            //For each row, iterate through all the columns
-            Iterator<Cell> cellIterator = row.cellIterator();
-             
-            while (cellIterator.hasNext()) 
-            {
-                Cell cell = cellIterator.next();
-                //Check the cell type and format accordingly
-                switch (cell.getCellType()) 
-                {
-                    case Cell.CELL_TYPE_NUMERIC:
-                        System.out.print(cell.getNumericCellValue() + "t");
-                        break;
-                    case Cell.CELL_TYPE_STRING:
-                        System.out.print(cell.getStringCellValue() + "t");
-                        break;
-                }
-            }
-            System.out.println("");
+
+            //TODO Create Configuration
+            Cell distributorCell = row.getCell(distributorsIndex);
+            result.getConfigurations().put(distributorCell.getStringCellValue(), new Configuration());
+            
+            //TODO Set Suffix
+            Cell suffixCell = row.getCell(suffixIndex);
+            result.getConfigurations().get(distributorCell.getStringCellValue()).setSuffix(suffixCell.getStringCellValue());
+            
+            //TODO Set ExternalCode column name
+            
+            //TODO Parse configuration
         }
 		
 		
